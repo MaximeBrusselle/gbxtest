bb4786f8578b7783413f50c2286559ed34f05a2b
Maxime Brusselle <brussma@cronos.be>
2024-06-26 00:28:19 +0200

    test to see if parsing works

--- api/index.py
+++ api/index.py
@@ -1,6 +1,16 @@
 from flask import Flask
+from gbx import Gbx, GbxType
 app = Flask(__name__)
 
 @app.route("/api/python")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("/api/testgbx")
+def test_gbx():
+    g = Gbx('test.Gbx')
+    ghost = g.get_class_by_id(GbxType.CTN_GHOST)
+    if not ghost:
+        quit()
+
+    return "<p>Ghost: " + ghost.name + "</p>"
